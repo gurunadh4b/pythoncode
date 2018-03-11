00a64f5ef0f48dd42969def72dea280af7c40549
gurunadh4b <gurunadh.4b@gmail.com>
2018-03-11 18:55:02 +0530

    added script to return files and dirs

---
+++ get_dir_files_in_dir.py
@@ -0,0 +1,45 @@
+'''from os import walk
+for dirpath,dirnames,filenames in walk('D:\MOVIES\ENGLISH'):
+    for i in filenames:
+        print(i)
+        
+'''
+
+
+l=[]
+from os import walk
+for dirpath,dirnames,filenames in walk('D:\Testing'):
+    for i in filenames:
+        l.append(i)
+print(l)
+  
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+    for i in x:
+        y=i.split(',')
+        print(y)
+'''
+    
+
+    
